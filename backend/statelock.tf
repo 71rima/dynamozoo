2daecebe5ca4a4274b8661db4d0e8bf630d2b753
Amir El-Shennawy <aelshennawy@tecracer.de>
2024-03-18 14:12:44 +0100

    init commit

---
+++ backend/statelock.tf
@@ -0,0 +1,12 @@
+resource "aws_dynamodb_table" "terraform-lock" {
+  name           = "terraform_state-dynamozoo"
+  read_capacity  = 5
+  write_capacity = 5
+  hash_key       = "LockID"
+  
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
---
+++ backend/statestorage.tf
@@ -0,0 +1,41 @@
+resource "aws_s3_bucket" "this" {
+  bucket              = "tfstate-dynamozoo1232523"
+  object_lock_enabled = true
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.bucket
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_object_lock_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    default_retention {
+      mode = "COMPLIANCE"
+      days = 5
+    }
+  }
+}
+
+resource "aws_kms_key" "this" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.this.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
